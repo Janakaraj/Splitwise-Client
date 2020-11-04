3f741c77a533c046ba74d26828b49db33d2d1f56
Janakaraj <janak@promactinfo.com>
2020-11-04 19:23:34 +0530

    Adds group-add feature

--- src/app/group/group-add/group-add.component.ts
+++ src/app/group/group-add/group-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { GroupAC, GroupClient, IGroupAC } from 'src/app/data.service';
 
 @Component({
   selector: 'app-group-add',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-add.component.css']
 })
 export class GroupAddComponent implements OnInit {
-
-  constructor() { }
+  @Output() loadDataEvent = new EventEmitter<boolean>();
+  mySubscription: any;
+  group: GroupAC = new GroupAC();
+  groupData: any = {
+    groupId: 0,
+    groupName: null,
+    groupCreatorId: null,
+    groupCreator: null
+  };
+  groupCreatorId: string = "ab9e1498-cf3a-4044-8e97-357d0df3d488";
+  constructor(private groupClient: GroupClient, private router:Router) {
+  }
 
   ngOnInit(): void {
   }
-
+  add() {
+    this.groupData.groupCreatorId = this.groupCreatorId;
+    this.group.init(this.groupData);
+    this.groupClient.groupPost(this.group).subscribe(result => {
+      console.log(result);
+      this.router.navigate(['/']);
+    },
+      error => console.error(error));
+  }
 }
