b8ddc3ddd4844322c974f70505874f25f42ddc4a
Janakaraj <janak@promactinfo.com>
2020-11-05 23:39:12 +0530

    Adds dynamic user friend list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GroupAC, UserGroupClient } from '../data.service';
+import { FriendClient, GroupAC, UserAC, UserGroupClient } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +8,25 @@ import { GroupAC, UserGroupClient } from '../data.service';
 })
 export class HomeComponent implements OnInit {
   groups: GroupAC[];
+  friends: UserAC[];
   userid: string = "ab9e1498-cf3a-4044-8e97-357d0df3d488";
-  constructor(private usergroupClient: UserGroupClient) { }
+  constructor(private usergroupClient: UserGroupClient, private friendClient: FriendClient) { }
   ngOnInit(): void {
     this.getGroupsByUserId(this.userid);
+    this.getFreindsByUserId(this.userid);
   }
   getGroupsByUserId(userid: string) {
-    this.usergroupClient.groupbyuserid(userid).subscribe(result => {
+    this.usergroupClient.getUserGroups(userid).subscribe(result => {
       this.groups = result;
       console.log(this.groups);
     },
       error => console.error(error));
+  }
+  getFreindsByUserId(userid: string){
+    this.friendClient.getFriends(userid).subscribe(result=>{
+      this.friends = result;
+      console.log(this.friends);
+    },
+    error=>console.error(error));
   }
 }
