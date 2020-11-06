c64381418916e6dddf62ccd68b6a5656b1b853d6
Janakaraj <janak@promactinfo.com>
2020-11-06 17:29:34 +0530

    Adds expense-edit feature

--- src/app/expense/expense-edit/expense-edit.component.ts
+++ src/app/expense/expense-edit/expense-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ExpenseAC, ExpenseClient } from 'src/app/data.service';
 
 @Component({
   selector: 'app-expense-edit',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expense-edit.component.css']
 })
 export class ExpenseEditComponent implements OnInit {
-
-  constructor() { }
+expenseId:number = 0;
+expenseToUpdate:ExpenseAC = new ExpenseAC();
+expense:any={
+  expenseId: this.expenseId,
+  expenseName: "string",
+  expenseTotalAmount: 0,
+  expenseGroupId: 0,
+  expenseSplitBy: "string",
+  expenseDescription: "string",
+  expenseCurrency: "string",
+  expenseAdderId: "string",
+};
+  constructor(private expenseClient: ExpenseClient, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(e => {
+    this.expenseId = +e.get('id');
+    });
+    this.expenseClient.getExpense(this.expenseId).subscribe(result => {
+      this.expense = result;
+      console.log(this.expense);
+    },
+    error => console.error(error));
+    //this.expenseClient.putExpense().subscribe();
   }
-
+update(){
+this.expenseToUpdate.init(this.expense);
+this.expenseClient.putExpense(this.expenseId,this.expenseToUpdate).subscribe(result => {
+  console.log(result);
+},
+error => console.error(error));
+}
 }
