8b7cf9c0ae7b413951ac00dd75d50456a1f0cc2d
Janakaraj <janak@promactinfo.com>
2020-11-11 22:37:21 +0530

    Implements remove-friend feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FriendClient, GroupAC, GroupClient, UserAC, UserGroupClient } from '../data.service';
+import { FriendClient, GroupAC, GroupClient, RemoveFriendAC, UserAC, UserGroupClient } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +10,10 @@ import { FriendClient, GroupAC, GroupClient, UserAC, UserGroupClient } from '../
 export class HomeComponent implements OnInit {
   groups: GroupAC[];
   friends: UserAC[];
+  friendToRemove: RemoveFriendAC = new RemoveFriendAC();
   userid: string = localStorage.getItem("userId");
   constructor(private usergroupClient: UserGroupClient, private friendClient: FriendClient,
-     private groupClient: GroupClient, private router:Router) { }
+    private groupClient: GroupClient, private router: Router) { }
   ngOnInit(): void {
     this.getGroupsByUserId(this.userid);
     this.getFreindsByUserId(this.userid);
@@ -30,12 +31,29 @@ export class HomeComponent implements OnInit {
       error => console.error(error));
   }
   deleteGroup(groupId: number) {
-    if (confirm("Are you sure to delete this group")) {
+    if (confirm("Are you sure to delete this group?")) {
       this.groupClient.deleteGroup(groupId).subscribe(() => {
         this.router.navigateByUrl(`/home/(dashboard//right:welcome)`)
-      .then(() => {
-        window.location.reload();
-      });
+          .then(() => {
+            window.location.reload();
+          });
+      },
+        error => console.error(error));
+    }
+  }
+  removeFriend(friendId: number) {
+    if (confirm("Are you sure you want to remove this friend?")) {
+      let removeFriendDetails = {
+        userId: this.userid,
+        userFriendId: friendId
+      };
+      console.log(removeFriendDetails);
+      this.friendToRemove.init(removeFriendDetails);
+      this.friendClient.removeFriend(this.friendToRemove).subscribe(() => {
+        this.router.navigateByUrl(`/home/(dashboard//right:welcome)`)
+          .then(() => {
+            window.location.reload();
+          });
       },
         error => console.error(error));
     }
