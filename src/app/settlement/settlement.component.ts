136a71fd926af495657fad3a67d3c6d5bef00749
Janakaraj <janak@promactinfo.com>
2020-11-13 11:23:27 +0530

    Adds non-group settlement

--- src/app/settlement/settlement.component.ts
+++ src/app/settlement/settlement.component.ts
@@ -11,11 +11,11 @@ export class SettlementComponent implements OnInit {
   showExpenses: boolean = false;
 
   constructor(private userGroupClient: UserGroupClient, private expenseClient: ExpenseClient,
-     private payerClient: PayerClient, private payeeClient: PayeeClient,
-      private settlementClient: SettlementClient, private router:Router) { }
+    private payerClient: PayerClient, private payeeClient: PayeeClient,
+    private settlementClient: SettlementClient, private router: Router) { }
   groupMembers: UserAC[] = [];
   userGroups: GroupAC[];
-  groupExpenses: ExpenseAC[];
+  groupExpenses: ExpenseAC[]=[];
   settlementAC: SettlementAC = new SettlementAC();
   payTo: string = '';
   paidBy: string = '';
@@ -24,24 +24,25 @@ export class SettlementComponent implements OnInit {
   expenseId: number = 0;
   userId: string = localStorage.getItem("userId");
   showUsers: boolean = false;
+  isNonGroupSettlement: boolean = false;
   ngOnInit(): void {
     this.getUserGroups();
   }
   getGroupMemebers() {
-    this.groupMembers = [];
-    this.payerClient.getPayersByExpenseId(this.expenseId).subscribe(result => {
-      for (let i = 0; i < result.length; i++) {
-        this.groupMembers.push(result[i].payerUser);
-      }
-      this.payeeClient.getPayeesByExpenseId(this.expenseId).subscribe(result => {
+      this.groupMembers = [];
+      this.payerClient.getPayersByExpenseId(this.expenseId).subscribe(result => {
         for (let i = 0; i < result.length; i++) {
-          this.groupMembers.push(result[i].payeeUser);
+          this.groupMembers.push(result[i].payerUser);
         }
+        this.payeeClient.getPayeesByExpenseId(this.expenseId).subscribe(result => {
+          for (let i = 0; i < result.length; i++) {
+            this.groupMembers.push(result[i].payeeUser);
+          }
+        },
+          error => console.error(error));
+        this.showUsers = true;
       },
         error => console.error(error));
-      this.showUsers = true;
-    },
-      error => console.error(error));
   }
   getUserGroups() {
     this.userGroupClient.getUserGroups(this.userId).subscribe(result => {
@@ -49,11 +50,23 @@ export class SettlementComponent implements OnInit {
     });
   }
   getGroupExpenses() {
+    if (this.groupId == -1) {
+      console.log("this is a non group settlement");
+      this.payerClient.getExpensesByPayerId(this.userId).subscribe(result=>{
+        for(let i=0;i<result.length;i++){
+          if(result[i].expense.expenseGroupId == null){
+            this.groupExpenses.push(result[i].expense);
+          }
+        }
+        this.showExpenses = true;
+      });
+    } else {
     this.expenseClient.getexpensesByGroupId(this.groupId).subscribe(result => {
       this.groupExpenses = result;
       this.showExpenses = true;
 
     });
+  }
   }
   add() {
     let settlementData = {
@@ -68,13 +81,24 @@ export class SettlementComponent implements OnInit {
       settlementExpense: null,
       transactionAmount: this.amount
     }
+    if(this.groupId == -1){
+      settlementData.settlementGroupId = null;
+    }
     this.settlementAC.init(settlementData);
-    this.settlementClient.postSettlement(this.settlementAC).subscribe(()=>{
-      this.router.navigateByUrl(`/home/(settlementList/${this.groupId}//right:listUser/${this.groupId})`)
-      .then(() => {
-        window.location.reload();
-      });
+    this.settlementClient.postSettlement(this.settlementAC).subscribe(() => {
+      if(this.groupId == -1){
+        this.router.navigateByUrl(`/home/(listExpense//right:welcome)`)
+        .then(() => {
+          window.location.reload();
+        });
+      }else{
+        this.router.navigateByUrl(`/home/(settlementList/${this.groupId}//right:listUser/${this.groupId})`)
+        .then(() => {
+          window.location.reload();
+        });
+      }
+      
     },
-    error=>console.error(error));
+      error => console.error(error));
   }
 }
